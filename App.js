517432d244dc888c3048389e9cff7e32aa4c3946
kothawaleganesh <kothawaleganesh@gmail.com>
2024-08-10 20:53:33 +0530

    Add initial React component rendering an H1 element

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this is h1 from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
